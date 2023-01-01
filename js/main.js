33737bbb387873d831d3b95fa85cc1eae52ec8aa
Stormfall13 <stormfall228@gmail.com>
2023-01-02 04:00:07 +0500

    and second form in russian

--- js/main.js
+++ js/main.js
@@ -66,67 +66,67 @@ $('.catalog__form').on('submit', function (event) {
 
 
 
-// $('.catalog__form').on('submit', function (event) {
+$('.popup__first').on('submit', function (event) {
 
-//     event.stopPropagation();
-//     event.preventDefault();
+    event.stopPropagation();
+    event.preventDefault();
 
-//     let form = this,
-//         submit = $('.submit', form),
-//         data = new FormData(),
-//         files = $('input[type=file]')
+    let form = this,
+        submit = $('.submit', form),
+        data = new FormData(),
+        files = $('input[type=file]')
 
 
-//     $('.submit', form).val('Отправка...');
-//     $('input, textarea', form).attr('disabled','');
+    $('.submit', form).val('Отправка...');
+    $('input, textarea', form).attr('disabled','');
 
-//     data.append( 'name', 		$('[name="name"]', form).val() );
-//     data.append( 'phone', 		$('[name="phone"]', form).val() );
-//     data.append( 'comment', 		$('[name="comment"]', form).val() );
+    data.append( 'Имя', 		$('[name="name"]', form).val() );
+    data.append( 'Телефон', 		$('[name="phone"]', form).val() );
+    data.append( 'Комментарий', 		$('[name="comment"]', form).val() );
     
 
-//     files.each(function (key, file) {
-//         let cont = file.files;
-//         if ( cont ) {
-//             $.each( cont, function( key, value ) {
-//                 data.append( key, value );
-//             });
-//         }
-//     });
+    files.each(function (key, file) {
+        let cont = file.files;
+        if ( cont ) {
+            $.each( cont, function( key, value ) {
+                data.append( key, value );
+            });
+        }
+    });
     
-//     $.ajax({
-//         url: 'ajax.php',
-//         type: 'POST',
-//         data: data,
-//         cache: false,
-//         dataType: 'json',
-//         processData: false,
-//         contentType: false,
-//         xhr: function() {
-//             let myXhr = $.ajaxSettings.xhr();
+    $.ajax({
+        url: 'ajax.php',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        xhr: function() {
+            let myXhr = $.ajaxSettings.xhr();
 
-//             if ( myXhr.upload ) {
-//                 myXhr.upload.addEventListener( 'progress', function(e) {
-//                     if ( e.lengthComputable ) {
-//                         let percentage = ( e.loaded / e.total ) * 100;
-//                             percentage = percentage.toFixed(0);
-//                         $('.submit', form)
-//                             .html( percentage + '%' );
-//                     }
-//                 }, false );
-//             }
+            if ( myXhr.upload ) {
+                myXhr.upload.addEventListener( 'progress', function(e) {
+                    if ( e.lengthComputable ) {
+                        let percentage = ( e.loaded / e.total ) * 100;
+                            percentage = percentage.toFixed(0);
+                        $('.submit', form)
+                            .html( percentage + '%' );
+                    }
+                }, false );
+            }
 
-//             return myXhr;
-//         },
-//         error: function( jqXHR, textStatus ) {
-//             // Тут выводим ошибку
-//         },
-//         complete: function() {
-//             // Тут можем что-то делать ПОСЛЕ успешной отправки формы
-//             console.log('Complete')
-//             form.reset() 
-//         }
-//     });
+            return myXhr;
+        },
+        error: function( jqXHR, textStatus ) {
+            // Тут выводим ошибку
+        },
+        complete: function() {
+            // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+            console.log('Complete')
+            form.reset() 
+        }
+    });
 
-//     return false;
-// });
+    return false;
+});
