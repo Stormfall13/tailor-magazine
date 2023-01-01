911f01be906ea55c2fabcbb9e5faf36630e3274e
Stormfall13 <stormfall228@gmail.com>
2023-01-02 03:12:49 +0500

    title message for tg in russian

--- js/main.js
+++ js/main.js
@@ -12,11 +12,11 @@ $('.catalog__form').on('submit', function (event) {
     $('.submit', form).val('Отправка...');
     $('input, textarea', form).attr('disabled','');
 
-    data.append( 'name', 		$('[name="name"]', form).val() );
-    data.append( 'phone', 		$('[name="phone"]', form).val() );
-    data.append( 'prod__name', 		$('[name="prod__name"]', form).val() );
-    data.append( 'size__prod', 		$('[name="size__prod"]', form).val() );
-    data.append( 'cena__prod', 		$('[name="cena__prod"]', form).val() );
+    data.append( 'Имя покупателя', 		$('[name="name"]', form).val() );
+    data.append( 'Номер телефона', 		$('[name="phone"]', form).val() );
+    data.append( 'Название товара', 		$('[name="prod__name"]', form).val() );
+    data.append( 'Размер', 		$('[name="size__prod"]', form).val() );
+    data.append( 'Цена', 		$('[name="cena__prod"]', form).val() );
 
     files.each(function (key, file) {
         let cont = file.files;
@@ -66,67 +66,67 @@ $('.catalog__form').on('submit', function (event) {
 
 
 
-$('.catalog__form').on('submit', function (event) {
+// $('.catalog__form').on('submit', function (event) {
 
-    event.stopPropagation();
-    event.preventDefault();
+//     event.stopPropagation();
+//     event.preventDefault();
 
-    let form = this,
-        submit = $('.submit', form),
-        data = new FormData(),
-        files = $('input[type=file]')
+//     let form = this,
+//         submit = $('.submit', form),
+//         data = new FormData(),
+//         files = $('input[type=file]')
 
 
-    $('.submit', form).val('Отправка...');
-    $('input, textarea', form).attr('disabled','');
+//     $('.submit', form).val('Отправка...');
+//     $('input, textarea', form).attr('disabled','');
 
-    data.append( 'name', 		$('[name="name"]', form).val() );
-    data.append( 'phone', 		$('[name="phone"]', form).val() );
-    data.append( 'comment', 		$('[name="comment"]', form).val() );
+//     data.append( 'name', 		$('[name="name"]', form).val() );
+//     data.append( 'phone', 		$('[name="phone"]', form).val() );
+//     data.append( 'comment', 		$('[name="comment"]', form).val() );
     
 
-    files.each(function (key, file) {
-        let cont = file.files;
-        if ( cont ) {
-            $.each( cont, function( key, value ) {
-                data.append( key, value );
-            });
-        }
-    });
+//     files.each(function (key, file) {
+//         let cont = file.files;
+//         if ( cont ) {
+//             $.each( cont, function( key, value ) {
+//                 data.append( key, value );
+//             });
+//         }
+//     });
     
-    $.ajax({
-        url: 'ajax.php',
-        type: 'POST',
-        data: data,
-        cache: false,
-        dataType: 'json',
-        processData: false,
-        contentType: false,
-        xhr: function() {
-            let myXhr = $.ajaxSettings.xhr();
+//     $.ajax({
+//         url: 'ajax.php',
+//         type: 'POST',
+//         data: data,
+//         cache: false,
+//         dataType: 'json',
+//         processData: false,
+//         contentType: false,
+//         xhr: function() {
+//             let myXhr = $.ajaxSettings.xhr();
 
-            if ( myXhr.upload ) {
-                myXhr.upload.addEventListener( 'progress', function(e) {
-                    if ( e.lengthComputable ) {
-                        let percentage = ( e.loaded / e.total ) * 100;
-                            percentage = percentage.toFixed(0);
-                        $('.submit', form)
-                            .html( percentage + '%' );
-                    }
-                }, false );
-            }
+//             if ( myXhr.upload ) {
+//                 myXhr.upload.addEventListener( 'progress', function(e) {
+//                     if ( e.lengthComputable ) {
+//                         let percentage = ( e.loaded / e.total ) * 100;
+//                             percentage = percentage.toFixed(0);
+//                         $('.submit', form)
+//                             .html( percentage + '%' );
+//                     }
+//                 }, false );
+//             }
 
-            return myXhr;
-        },
-        error: function( jqXHR, textStatus ) {
-            // Тут выводим ошибку
-        },
-        complete: function() {
-            // Тут можем что-то делать ПОСЛЕ успешной отправки формы
-            console.log('Complete')
-            form.reset() 
-        }
-    });
+//             return myXhr;
+//         },
+//         error: function( jqXHR, textStatus ) {
+//             // Тут выводим ошибку
+//         },
+//         complete: function() {
+//             // Тут можем что-то делать ПОСЛЕ успешной отправки формы
+//             console.log('Complete')
+//             form.reset() 
+//         }
+//     });
 
-    return false;
-});
+//     return false;
+// });
